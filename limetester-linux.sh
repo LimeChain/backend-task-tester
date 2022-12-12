4f101f420698cb26bf960766fadc3a7d6e7880f0
George Spasov <george@limechain.tech>
2022-12-12 15:35:01 +0200

    Removed hardcoded API_PORT param

--- limetester-linux.sh
+++ limetester-linux.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/LimeChain/backend-task-tester/raw/main/limebackendtester-linux
 chmod u+x ./limebackendtester-linux
-API_PORT=3061 ETH_NODE_URL="https://eth-goerli.g.alchemy.com/v2/tbOepyPHC7Bqub--gHJzkZr97UT16cSz" DB_CONNECTION_URL="postgresql://postgres:myzkevmpassword@localhost:5432/postgres" ./limebackendtester-linux
+ETH_NODE_URL="https://eth-goerli.g.alchemy.com/v2/tbOepyPHC7Bqub--gHJzkZr97UT16cSz" DB_CONNECTION_URL="postgresql://postgres:myzkevmpassword@localhost:5432/postgres" ./limebackendtester-linux
 rm ./limebackendtester-linux
