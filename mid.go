25014b01a8ba9e2b8ab09112e833eb73b15dd1ee
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2024-02-08 17:59:37 +0200

    Check transactions in testExampleTxFetching without depending on their order
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- mid.go
+++ mid.go
@@ -34,98 +34,117 @@ func testExampleTxFetching(rpcClient *LimeClient) testable {
 			return false
 		}
 
-		if res.Transactions[0].TransactionStatus != 1 {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong tx status on index %d\n", 0)
+		txns := map[string]Transaction{}
+		for i := 0; i < len(res.Transactions); i++ {
+			txns[res.Transactions[i].TransactionHash] = res.Transactions[i]
+		}
+
+		txnHash := "0x9b2f6a3c2e1aed2cccf92ba666c22d053ad0d8a5da7aa1fd5477dcd6577b4524"
+		if txn, ok := txns[txnHash]; ok {
+			if txn.TransactionStatus != 1 {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong tx status for %s\n", txnHash)
+				return false
+			}
+
+			if txn.BlockNumber != 7976382 {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong block number for %s\n", txnHash)
+				return false
+			}
+
+			if txn.To != "" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong to address for %s\n", txnHash)
+				return false
+			}
+
+			if strings.ToLower(txn.From) != "0xb4d6a98aa8cd5396069c2818adf4ae1a0384b43a" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong from address for %s\n", txnHash)
+				return false
+			}
+		} else {
+			log.Printf("[testExampleTxFetching] FAIL: Didn't find expected txn for %s\n", txnHash)
 			return false
 		}
 
-		if res.Transactions[1].TransactionStatus != 1 {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong tx status on index %d\n", 3)
+		txnHash = "0x5a57e3051cb92e2d482515b07e7b3d1851722a74654657bd64a14c39ca3f9cf2"
+		if txn, ok := txns[txnHash]; ok {
+			if txn.TransactionStatus != 1 {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong tx status for %s\n", txnHash)
+				return false
+			}
+
+			if strings.ToLower(txn.BlockHash) != "0x92557f7e29c39cae6be013ffc817620fcd5233b68405cdfc6e0b5528261e81e5" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong block hash for %s\n", txnHash)
+				return false
+			}
+
+			if strings.ToLower(txn.From) != "0xf29a6c0f8ee500dc87d0d4eb8b26a6fac7a76767" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong from address for %s\n", txnHash)
+				return false
+			}
+
+			if strings.ToLower(txn.To) != "0xb0428bf0d49eb5c2239a815b43e59e124b84e303" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong to address for %s\n", txnHash)
+				return false
+			}
+
+			if txn.Input != "0x" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong input for %s\n", txnHash)
+				return false
+			}
+
+			if txn.Value != 50000000000000000 {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong value for %s\n", txnHash)
+				return false
+			}
+		} else {
+			log.Printf("[testExampleTxFetching] FAIL: Didn't find expected txn for %s\n", txnHash)
 			return false
 		}
 
-		if res.Transactions[3].TransactionStatus != 0 {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong tx status on index %d\n", 3)
+		txnHash = "0x71b9e2b44d40498c08a62988fac776d0eac0b5b9613c37f9f6f9a4b888a8b057"
+		if txn, ok := txns[txnHash]; ok {
+			if txn.BlockNumber != 7957369 {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong block number for %s\n", txnHash)
+				return false
+			}
+
+			if txn.LogsCount != 3 {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong logs count for %s\n", txnHash)
+				return false
+			}
+		} else {
+			log.Printf("[testExampleTxFetching] FAIL: Didn't find expected txn for %s\n", txnHash)
 			return false
 		}
 
-		if strings.ToLower(res.Transactions[0].TransactionHash) != "0x9b2f6a3c2e1aed2cccf92ba666c22d053ad0d8a5da7aa1fd5477dcd6577b4524" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong tx hash on index %d\n", 0)
-			return false
-		}
+		txnHash = "0xc5f96bf1b54d3314425d2379bd77d7ed4e644f7c6e849a74832028b328d4d798"
+		if txn, ok := txns[txnHash]; ok {
+			if txn.TransactionStatus != 0 {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong tx status for %s\n", txnHash)
+				return false
+			}
 
-		if strings.ToLower(res.Transactions[2].TransactionHash) != "0x71b9e2b44d40498c08a62988fac776d0eac0b5b9613c37f9f6f9a4b888a8b057" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong tx hash on index %d\n", 2)
-			return false
-		}
+			if strings.ToLower(txn.BlockHash) != "0x3ac55cb392661e0d2239267022dc30f32dc4767cdacfd3e342443122b87101d3" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong block hash for %s\n", txnHash)
+				return false
+			}
 
-		if strings.ToLower(res.Transactions[1].BlockHash) != "0x92557f7e29c39cae6be013ffc817620fcd5233b68405cdfc6e0b5528261e81e5" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong block hash on index %d\n", 1)
-			return false
-		}
+			if strings.ToLower(txn.From) != "0x58fa6ab2931b73a22d85617125b936bd3f74e765" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong from address for %s\n", txnHash)
+				return false
+			}
 
-		if strings.ToLower(res.Transactions[3].BlockHash) != "0x3ac55cb392661e0d2239267022dc30f32dc4767cdacfd3e342443122b87101d3" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong block hash on index %d\n", 3)
-			return false
-		}
+			if strings.ToLower(txn.To) != "0x302fd86163cb9ad5533b3952dafa3b633a82bc51" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong to address for %s\n", txnHash)
+				return false
+			}
 
-		if res.Transactions[0].BlockNumber != 7976382 {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong block number on index %d\n", 0)
-			return false
-		}
-
-		if res.Transactions[2].BlockNumber != 7957369 {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong block number on index %d\n", 2)
-			return false
-		}
-
-		if strings.ToLower(res.Transactions[0].From) != "0xb4d6a98aa8cd5396069c2818adf4ae1a0384b43a" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong from index %d\n", 0)
-			return false
-		}
-
-		if res.Transactions[0].To != "" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong to index %d\n", 0)
-			return false
-		}
-
-		if strings.ToLower(res.Transactions[1].From) != "0xf29a6c0f8ee500dc87d0d4eb8b26a6fac7a76767" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong from index %d\n", 1)
-			return false
-		}
-
-		if strings.ToLower(res.Transactions[1].To) != "0xb0428bf0d49eb5c2239a815b43e59e124b84e303" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong to index %d\n", 1)
-			return false
-		}
-
-		if strings.ToLower(res.Transactions[3].From) != "0x58fa6ab2931b73a22d85617125b936bd3f74e765" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong from index %d\n", 3)
-			return false
-		}
-
-		if strings.ToLower(res.Transactions[3].To) != "0x302fd86163cb9ad5533b3952dafa3b633a82bc51" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong to index %d\n", 3)
-			return false
-		}
-
-		if res.Transactions[1].Input != "0x" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong input index %d\n", 1)
-			return false
-		}
-
-		if res.Transactions[1].Value != "50000000000000000" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong value index %d\n", 1)
-			return false
-		}
-
-		if res.Transactions[3].Input != "0x97da873c0000000000000000000000000000000000000000000000056bc75e2d63100000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000058fa6ab2931b73a22d85617125b936bd3f74e76512d1a55b318c0be714e7ce8bc54a96ac48813cfcb73cbaa0a6e933fa9a35b7bb212c8f9a45c4430a6fa3cb8b67a28403c51e494615df4f826280256a8ddabde630818902818100e4dcd34866228be9255cbd322590b92ded49868321f0535734587348c4cb450d2d68367f686faa4688410662e9f38dc62a742f71d8e81b40a3c444381ee1245024467c8f29f04f0f83059dee234f1d4ab13e536eb5958adf91782ed3495b36fd5db6e76626771d998d6e4c75eceb58e1c783b33920dcd7723fbfbc33ba6d5ff902030100010000000000000000000000000000000000000000" {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong input index %d\n", 3)
-			return false
-		}
-
-		if res.Transactions[2].LogsCount != 3 {
-			log.Printf("[testExampleTxFetching] FAIL: Wrong logs count on index %d\n", 2)
+			if txn.Input != "0x97da873c0000000000000000000000000000000000000000000000056bc75e2d63100000000000000000000000000000000000000000000000000000000000000000000400000000000000000000000058fa6ab2931b73a22d85617125b936bd3f74e76512d1a55b318c0be714e7ce8bc54a96ac48813cfcb73cbaa0a6e933fa9a35b7bb212c8f9a45c4430a6fa3cb8b67a28403c51e494615df4f826280256a8ddabde630818902818100e4dcd34866228be9255cbd322590b92ded49868321f0535734587348c4cb450d2d68367f686faa4688410662e9f38dc62a742f71d8e81b40a3c444381ee1245024467c8f29f04f0f83059dee234f1d4ab13e536eb5958adf91782ed3495b36fd5db6e76626771d998d6e4c75eceb58e1c783b33920dcd7723fbfbc33ba6d5ff902030100010000000000000000000000000000000000000000" {
+				log.Printf("[testExampleTxFetching] FAIL: Wrong input for %s\n", txnHash)
+				return false
+			}
+		} else {
+			log.Printf("[testExampleTxFetching] FAIL: Didn't find expected txn for %s\n", txnHash)
 			return false
 		}
 
