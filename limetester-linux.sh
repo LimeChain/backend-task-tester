3e1fc6e409aefba5b80b53f184f803d3957876e8
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2024-02-15 16:02:47 +0200

    Update shell scripts
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- limetester-linux.sh
+++ limetester-linux.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/LimeChain/backend-task-tester/raw/main/limebackendtester-linux
 chmod u+x ./limebackendtester-linux
-ETH_NODE_URL="https://eth-goerli.g.alchemy.com/v2/tbOepyPHC7Bqub--gHJzkZr97UT16cSz" DB_CONNECTION_URL="postgresql://postgres:myzkevmpassword@localhost:5432/postgres" ./limebackendtester-linux
+./limebackendtester-linux
 rm ./limebackendtester-linux
--- limetester-osx.sh
+++ limetester-osx.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/LimeChain/backend-task-tester/raw/main/limebackendtester-osx
 chmod u+x ./limebackendtester-osx
-ETH_NODE_URL="https://eth-goerli.g.alchemy.com/v2/tbOepyPHC7Bqub--gHJzkZr97UT16cSz" DB_CONNECTION_URL="postgresql://postgres:myzkevmpassword@localhost:5432/postgres" ./limebackendtester-osx
+./limebackendtester-osx
 rm ./limebackendtester-osx
