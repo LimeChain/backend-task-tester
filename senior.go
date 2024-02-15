00f6dddd96ba56f69f0787c5958543f8abff58c3
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2024-02-15 15:51:51 +0200

    Remove empty lines
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- senior.go
+++ senior.go
@@ -32,30 +32,25 @@ var expectedMyTxns = []Transaction{
 func testAuthenticate(rpcClient *LimeClient) testable {
 	return func() bool {
 		res, err := rpcClient.PostAuthenticate("carol", "carol")
-
 		if err != nil || res == nil || len(res.Token) == 0 {
 			log.Println("[testAuthenticate] FAIL: No response for carol")
 			return false
 		}
 
 		carolToken := res.Token
-
 		res, err = rpcClient.PostAuthenticate("dave", "dave")
-
 		if err != nil || res == nil || len(res.Token) == 0 {
 			log.Println("[testAuthenticate] FAIL: No response for dave")
 			return false
 		}
 
 		daveToken := res.Token
-
 		if carolToken == daveToken {
 			log.Println("[testAuthenticate] FAIL: Carol and Dave got the same token")
 			return false
 		}
 
 		_, err = rpcClient.PostAuthenticate("george", "george")
-
 		if err == nil {
 			log.Println("[testAuthenticate] FAIL: george got a token, but should not have done so")
 			return false
@@ -68,19 +63,15 @@ func testAuthenticate(rpcClient *LimeClient) testable {
 
 func testGetMyTransactions(rpcClient *LimeClient) testable {
 	return func() bool {
-
 		authRes, err := rpcClient.PostAuthenticate("carol", "carol")
-
 		if err != nil || authRes == nil || len(authRes.Token) == 0 {
 			log.Println("[testAuthenticate] FAIL: No response for carol")
 			return false
 		}
 
 		carolToken := authRes.Token
-
 		rlpString := "/f888b842307834343462383533333036623361346639333463393063623939363935363936366438303465646332376662623637343032656262383239346132636338666432b842307830346663663665396636343531313633613233316363376235663961653462323833356466656237666632636235643239363039333433663039613330663739"
 		res, err := rpcClient.GetEth(rlpString, carolToken)
-
 		if err != nil || res == nil {
 			log.Println("[testGetMyTransactions] FAIL: No response")
 			return false
@@ -113,7 +104,6 @@ func testGetMyTransactions(rpcClient *LimeClient) testable {
 
 func testDockerfileExists() testable {
 	return func() bool {
-
 		if _, err := os.Stat("./Dockerfile"); errors.Is(err, os.ErrNotExist) {
 			log.Println("[testDockerfileExists] FAIL: Dockerfile does not exist")
 			return false
